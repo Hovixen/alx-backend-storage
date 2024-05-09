51e7c8fa43e09d138aaa9e1d2979f61467fa72e4
Hovixen <orevaerijo@gmail.com>
2024-05-09 19:54:57 +0000

    task 9

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -3,4 +3,4 @@
 
 DROP INDEX IF EXISTS idx_name_first_score ON names;
 
-CREATE INDEX idx_name_first_score ON names(LEFT(name, 1), score);
+CREATE INDEX idx_name_first_score ON names((name, 1), score);
