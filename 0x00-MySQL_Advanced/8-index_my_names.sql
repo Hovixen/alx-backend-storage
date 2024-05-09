f6d92eceebd0ffe7785f4d90e534cae46ac3763e
Hovixen <orevaerijo@gmail.com>
2024-05-09 21:03:22 +0000

    task 8

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,5 +1,4 @@
 -- Script creates an index idx_name_first on the table names
 -- and the first letter of name and the score
 
-
-CREATE UNIQUE INDEX idx_name_first_score ON names(name(1)) USING BTREE;
+CREATE INDEX idx_name_first ON names(name(1));
